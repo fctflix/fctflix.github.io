4ed65d1feab7ee0ee71fd5cc8449f3ed6b875b5d
dm-carmo <dm.carmo@campus.fct.unl.pt>
2017-11-08 12:57:31 +0000

    stuff and things

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 function showNotifs() {
     $("#notif_popup").toggleClass("showpopup");
+    $("#user_popup").removeClass("showpopup");
 }
 
 function showDefs() {
     $("#user_popup").toggleClass("showpopup");
+    $("#notif_popup").removeClass("showpopup");
 }
